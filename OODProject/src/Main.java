b13fb92a911b2caf36294c9ed5d83bb558710837
Andrew <Andrew@ALSANDERS>
2019-04-02 14:39:17 -0400

    Added Javafx Green Scene

--- OODProject/src/Main.java
+++ OODProject/src/Main.java
@@ -1,10 +1,22 @@
-
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+public class Main extends Application{
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
-		
-
+		launch(args);
 	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("ZombiLand");
+		Pane pane = new Pane();
+		Scene scene = new Scene(pane, 300, 250, Color.GREEN);
+		
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 }
