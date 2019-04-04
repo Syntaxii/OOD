5f52f6875c19b4a3b5b1b97a4474c55bba69fd79
as13770 <as13770@CEIT2552212X002.ad.georgiasouthern.edu>
2019-04-04 14:59:31 -0400

    bid gick

--- OODProject/src/Main.java
+++ OODProject/src/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application{
 	private Rectangle mouseCursor1, mouseCursor2, mouseCursor3, mouseCursor4;
 	boolean goUp, goDown, goRight, goLeft;
 	private bulletHandling bHandler;
-	private double centerOffsetX, centerOffsetY;
+	private double centerOffsetX, centerOffsetY, mouseX, mouseY;
 	private Circle shadow;
 
 
@@ -30,8 +30,10 @@ public class Main extends Application{
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		
+
 		bHandler = new bulletHandling();
+		mouseX = 0.0;
+		mouseY = 0.0;
 
 		playerImage = new Image(imgURL);
 		playerImage.getWidth();
@@ -41,41 +43,41 @@ public class Main extends Application{
 		player.setScaleY(.4);
 		centerOffsetX = (playerImage.getWidth())/2;
 		centerOffsetY = (playerImage.getHeight())/2;
-		
-		
+
+
 		//TODO condense
 		mouseCursor1 = new Rectangle(10, 5);
 		mouseCursor1.setRotate(45);
 		mouseCursor1.setFill(Color.FLORALWHITE);
 		mouseCursor1.setStroke(Color.BLACK);
-		
+
 		mouseCursor2 = new Rectangle(10, 5);
 		mouseCursor2.setRotate(135);
 		mouseCursor2.setFill(Color.FLORALWHITE);
 		mouseCursor2.setStroke(Color.BLACK);
-		
+
 		mouseCursor3 = new Rectangle(10, 5);
 		mouseCursor3.setRotate(225);
 		mouseCursor3.setFill(Color.FLORALWHITE);
 		mouseCursor3.setStroke(Color.BLACK);
-		
+
 		mouseCursor4 = new Rectangle(10, 5);
 		mouseCursor4.setRotate(315);
 		mouseCursor4.setFill(Color.FLORALWHITE);
 		mouseCursor4.setStroke(Color.BLACK);
-		
+
 		BorderPane root = new BorderPane();
-		
+
 		Pane floor = new Pane(player);
-		
+
 		root.getChildren().add(floor);
 		floor.getChildren().addAll(mouseCursor1, mouseCursor2, mouseCursor3, mouseCursor4);
-		
+
 		moveTo(width/2, height/2);
-		
+
 		Scene scene = new Scene(root, width, height);
 		scene.setCursor(Cursor.NONE);
-		
+
 		BackgroundImage myBI= new BackgroundImage(new Image("http://www.dundjinni.com/forums/uploads/Eanwulf/237_Sample.jpg",750,700,false,true),
 				BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
 				BackgroundSize.DEFAULT);
@@ -118,42 +120,42 @@ public class Main extends Application{
 		});
 		scene.setOnMouseMoved(new EventHandler<MouseEvent> () {
 			public void handle(MouseEvent event) {
-				double MouseX = event.getX();
-				double MouseY = event.getY();
-				moveMouse(MouseX, MouseY);
-				rotatePlayer(MouseX, MouseY);
-				
+				mouseX = event.getX();
+				mouseY = event.getY();
+				moveMouse();
+				rotatePlayer();
+
 				System.out.println("PlayerX: " + player.getBoundsInParent().getMinX() + (player.getBoundsInParent().getMaxX()-player.getBoundsInParent().getMinX())/2 + " MouseX: " + event.getX());
 			}
 		});
 		scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
 			public void handle(MouseEvent e) {
-				double MouseX = e.getX();
-				double MouseY = e.getY();
-				moveMouse(MouseX, MouseY);
-				rotatePlayer(MouseX, MouseY);
+				mouseX = e.getX();
+				mouseY = e.getY();
+				moveMouse();
+				rotatePlayer();
 
 			}
 		});
-		
+
 		scene.setOnMouseReleased(new EventHandler<MouseEvent>() {
 			public void handle(MouseEvent e) {
 				setMouseColor(Color.FLORALWHITE);
 			}
 		});
-		
+
 		scene.setOnMousePressed(new EventHandler<MouseEvent>() {
 			public void handle(MouseEvent e) {
 				double MouseX = e.getX();
 				double MouseY = e.getY();		
 				setMouseColor(Color.SALMON);
 
-		//		double cx = player.getBoundsInLocal().getWidth()  / 2;
+				//		double cx = player.getBoundsInLocal().getWidth()  / 2;
 				double cx = player.getLayoutX();
 
-		//		double cy = player.getBoundsInLocal().getHeight() / 2;
+				//		double cy = player.getBoundsInLocal().getHeight() / 2;
 				double cy = player.getLayoutY();
-				
+
 				System.out.println(MouseX + " MouseX\n " + MouseY + " MouseY\n " + cx + " cx\n " + cy + " cy\n");
 				createBullet(MouseX, MouseY, cx+centerOffsetX, cy+centerOffsetY, floor);
 
@@ -201,39 +203,30 @@ public class Main extends Application{
 					offsetAmount = getXandY(currentAngle);
 					moveBy(offsetAmount[0]*5, offsetAmount[1]*5);
 				}
-				
+
 				bHandler.cycleProjectiles();
-			
+				rotatePlayer();
+
 			}
 		};
 		timer.start();
-		
-		
+
+
 	}
-	
+
 	private void createBullet(double mouseX, double mouseY, double cx, double cy, Pane floor) {
 		Bullet pBullet = new Bullet(mouseX,mouseY,cx,cy);
 		bHandler.addProjectile(pBullet);
 		floor.getChildren().add(pBullet.getBullet());
-		
+
 	}
-	
+
 	// make a function to make player face the direction of the mouse more fluidly
-	private void rotatePlayer(double x, double y) {
-		if(x < player.getBoundsInParent().getMinX() + (player.getBoundsInParent().getMaxX() - player.getBoundsInParent().getMinX())/2) {
-			//player.setScaleX(Math.abs(player.getScaleX()));
-			player.setRotate(180);
-		}
-		else if(y < player.getBoundsInParent().getMinY() + (player.getBoundsInParent().getMaxY() - player.getBoundsInParent().getMinY())/2) {
-			player.setRotate(270);
-		}
-		else if(y > player.getBoundsInParent().getMaxY() + (player.getBoundsInParent().getMaxY() - player.getBoundsInParent().getMinY())/2) {
-			player.setRotate(90);
-		}
-		else {
-			//player.setScaleX(Math.abs(player.getScaleX()) * -1);
-			player.setRotate(0);
-		}
+	private void rotatePlayer() {
+		double playerPosx = player.getBoundsInParent().getMinX() + (player.getBoundsInParent().getMaxX()-player.getBoundsInParent().getMinX())/2;
+		double playerPosy = player.getBoundsInParent().getMinY() + (player.getBoundsInParent().getMaxY()-player.getBoundsInParent().getMinY())/2;
+		double angle = Math.atan2(mouseY - playerPosy, mouseX - playerPosx) * 180 / Math.PI;
+		player.setRotate(angle);
 	}
 
 	private void moveBy(double dx, double dy) {
@@ -260,29 +253,29 @@ public class Main extends Application{
 		double newY = Math.sin(radianAngle);
 		return new double[] {newX, newY};
 	}
-	
-	private void moveMouse(double x, double y) {
+
+	private void moveMouse() {
 		int d = 10; //displacement. Easier to change with a single variable
-		mouseCursor1.setX(x+d-5);
-		mouseCursor1.setY(y+d-2);
-		
-		mouseCursor2.setX(x+d-5);
-		mouseCursor2.setY(y-d-2);
-		
-		mouseCursor3.setX(x-d-5);
-		mouseCursor3.setY(y-d-2);
-		
-		mouseCursor4.setX(x-d-5);
-		mouseCursor4.setY(y+d-2);
+		mouseCursor1.setX(mouseX+d-5);
+		mouseCursor1.setY(mouseY+d-2);
+
+		mouseCursor2.setX(mouseX+d-5);
+		mouseCursor2.setY(mouseY-d-2);
+
+		mouseCursor3.setX(mouseX-d-5);
+		mouseCursor3.setY(mouseY-d-2);
+
+		mouseCursor4.setX(mouseX-d-5);
+		mouseCursor4.setY(mouseY+d-2);
 	}
-	
+
 	private void setMouseColor(Paint color) {
 		mouseCursor1.setFill(color);
-		
+
 		mouseCursor2.setFill(color);
-		
+
 		mouseCursor3.setFill(color);
-		
+
 		mouseCursor4.setFill(color);
 	}
 }
