5da47a5659a7da3fdc48c2185e00ce08ffb5a1e8
Justin <Justin@192.168.2.3>
2019-04-11 18:20:02 -0400

    Basic enemy abstract

--- OODProject/src/Enemy.java
+++ OODProject/src/Enemy.java
@@ -1,4 +1,19 @@
-
 public abstract class Enemy {
-
+	double enemyX, enemyY, playerX, playerY;
+	double eVelosity;
+	boolean alive = true;
+	int health;
+	
+	public void spawn() {
+		
+	}
+	public void move() {
+		
+	}
+	public void damage() {
+		
+	}
+	public void attack() {
+		
+	}
 }
